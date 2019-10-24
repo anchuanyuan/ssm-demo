b474d00921b9b41ff4cc737209b53ea939aa4376
acy <1162734640@qq.com>
2019-10-24 22:02:54 +0800

    添加一个分支

---
+++ src/main/java/com/example/comunity/utils/CookiesManager.java
@@ -0,0 +1,19 @@
+package com.example.comunity.utils;
+
+import com.example.comunity.mapper.UserMapper;
+import com.example.comunity.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Auther: An
+ * @Date:2019/10/24
+ * @Description:com.example.comunity.utils
+ * @version:1.0
+ */
+public class CookiesManager {
+
+
+}
